e0e3881cde92df85ff546fd6f935efcbb92f70e3
Guimaaz <gustavoguimaraesr@gmail.com>
2025-03-27 12:42:04 -0300

    vetor funcional

---
+++ NossoVetor.java
@@ -0,0 +1,71 @@
+public class NossoVetor<T> {
+  private Object[] vetor;
+  private int tamanho = 0;
+
+  public NossoVetor(int capacidade) {
+      this.vetor = new Object[capacidade];
+  }
+
+  public void adicionar(int posicao, T elemento) {
+      if (!(posicao >= 0 && posicao <= this.tamanho)) {
+          System.out.println("Posição inválida");
+          return;
+      }
+
+      aumentarCapacidade();
+
+      for (int i = this.tamanho - 1; i >= posicao; i--) {
+          this.vetor[i + 1] = this.vetor[i];
+      }
+
+      this.vetor[posicao] = elemento;
+      this.tamanho++;
+  }
+
+  public int getPosicaoDoElemento(T elemento) {
+      for (int i = 0; i < this.tamanho; i++) {
+          if (this.vetor[i].equals(elemento)) { 
+              return i;
+          }
+      }
+      return -1;
+  }
+
+  public void removerPosicao(int posicao) {
+      if (!(posicao >= 0 && posicao <= this.tamanho)) { 
+          System.out.println("Posição inválida");
+          return;
+      }
+
+      for (int i = posicao; i < this.tamanho - 1; i++) {
+          this.vetor[i] = this.vetor[i + 1]; 
+      }
+
+      this.vetor[this.tamanho - 1] = null; 
+      this.tamanho--;
+  }
+
+  
+  @SuppressWarnings("unchecked")
+  private void aumentarCapacidade() {
+      if (this.tamanho == this.vetor.length) {
+          Object[] vetorAumentado =(T[]) new Object[this.vetor.length * 2];
+          for (int i = 0; i < this.vetor.length; i++) {
+              vetorAumentado[i] = this.vetor[i];
+          }
+          this.vetor = vetorAumentado;
+      }
+  }
+public String toString(){
+  StringBuilder s = new StringBuilder();
+  s.append("[");
+  for( int i = 0; i < this.tamanho; i++){
+    s.append(this.vetor[i]);
+    s.append(",");
+
+  }
+  s.append("]");
+
+  return s.toString();
+}
+}
---
+++ Teste1.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class Teste1 {
+    public static void main(String[] args) {
+        int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Qual o tamanho do vetor?"));
+        NossoVetor<Integer> vetor = new NossoVetor<>(tamanho);
+
+        int opcao;
+        do {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Escolha uma opção:\n1 - Inserir\n2 - Remover\n3 - Exibir\n0 - Sair"));
+
+            switch (opcao) {
+                case 1:
+                    int posicao = Integer.parseInt(JOptionPane.showInputDialog("Digite a posição que quer inserir no vetor:"));
+                    int elemento = Integer.parseInt(JOptionPane.showInputDialog("Digite o elemento a ser inserido:"));
+                    vetor.adicionar(posicao, elemento);
+                    break;
+                case 2:
+                    int posicaoRemover = Integer.parseInt(JOptionPane.showInputDialog("Digite a posição a ser removida:"));
+                    vetor.removerPosicao(posicaoRemover);
+                    break;
+                case 3:
+                    System.out.println(vetor.toString());
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Saindo...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida!");
+            }
+        } while (opcao != 0);
+    }
+}
