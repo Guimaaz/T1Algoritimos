db1b72adb2762bf0e67f760d920f21435d1ab2e2
GuiMD <gmdurt14@gmail.com>
2025-03-31 15:15:19 -0300

    Adição do insertion e selection sort, com os contadores de operação

--- NossoVetor.java
+++ NossoVetor.java
@@ -1,70 +1,70 @@
 import java.util.Random;
 
 public class NossoVetor {
-  private int [] vetor;
-  private int tamanho = 0;
+    private int [] vetor;
+    private int tamanho = 0;
 
-  public NossoVetor(int capacidade) {
-      this.vetor = new int[capacidade];
-  }
+public NossoVetor(int capacidade) {
+    this.vetor = new int[capacidade];
+}
 
-  public void adicionar(int posicao, int elemento) {
-      if (!(posicao >= 0 && posicao <= this.tamanho)) {
-          System.out.println("Posição inválida");
-          return;
-      }
+public void adicionar(int posicao, int elemento) {
+    if (!(posicao >= 0 && posicao <= this.tamanho)) {
+        System.out.println("Posição inválida");
+        return;
+    }
 
-      aumentarCapacidade();
+    aumentarCapacidade();
 
-      for (int i = this.tamanho - 1; i >= posicao; i--) {
-          this.vetor[i + 1] = this.vetor[i];
-      }
+    for (int i = this.tamanho - 1; i >= posicao; i--) {
+        this.vetor[i + 1] = this.vetor[i];
+    }
 
-      this.vetor[posicao] = elemento;
-      this.tamanho++;
-  }
+    this.vetor[posicao] = elemento;
+    this.tamanho++;
+}
 
-  public int getPosicaoDoElemento(int elemento) {
-      for (int i = 0; i < this.tamanho; i++) {
-          if (this.vetor[i] == elemento ) { 
-              return i;
-          }
-      }
-      return -1;
-  }
+public int getPosicaoDoElemento(int elemento) {
+    for (int i = 0; i < this.tamanho; i++) {
+        if (this.vetor[i] == elemento ) { 
+            return i;
+        }
+    }
+    return -1;
+}
 
-  public void removerPosicao(int posicao) {
-      if (!(posicao >= 0 && posicao <= this.tamanho)) { 
-          System.out.println("Posição inválida");
-          return;
-      }
+public void removerPosicao(int posicao) {
+    if (!(posicao >= 0 && posicao <= this.tamanho)) { 
+        System.out.println("Posição inválida");
+        return;
+    }
 
-      for (int i = posicao; i < this.tamanho - 1; i++) {
-          this.vetor[i] = this.vetor[i + 1]; 
-      }
+    for (int i = posicao; i < this.tamanho - 1; i++) {
+        this.vetor[i] = this.vetor[i + 1]; 
+    }
  
-      this.tamanho--;
-  }
+    this.tamanho--;
+}
   
- 
-  private void aumentarCapacidade() {
-      if (this.tamanho == this.vetor.length) {
-          int [] vetorAumentado = new int[this.vetor.length * 2];
-          for (int i = 0; i < this.vetor.length; i++) {
-              vetorAumentado[i] = this.vetor[i];
-          }
-          this.vetor = vetorAumentado;
-      }
-  }
-public String toString(){
-  StringBuilder s = new StringBuilder();
-  s.append("[");
-  for( int i = 0; i < this.tamanho; i++){
-    s.append(this.vetor[i]);
-    s.append(",");
+private void aumentarCapacidade() {
+    if (this.tamanho == this.vetor.length) {
+        int [] vetorAumentado = new int[this.vetor.length * 2];
+        for (int i = 0; i < this.vetor.length; i++) {
+            vetorAumentado[i] = this.vetor[i];
+        }
+        this.vetor = vetorAumentado;
+    }
+}
 
-  }
-  s.append("]");
+public String toString(){
+    StringBuilder s = new StringBuilder();
+    s.append("[");
+    for( int i = 0; i < this.tamanho; i++){
+        s.append(this.vetor[i]);
+        s.append(",");
+
+    }
+    s.append("]");
 
   return s.toString();
 }
@@ -75,7 +75,6 @@ public void preencheVetor() {
         this.vetor[i] = random.nextInt(vetor.length * 10);
     }
 tamanho = this.vetor.length;
-
 }
 
 public void BubbleSort() {
@@ -102,5 +101,51 @@ public void BubbleSort() {
     System.out.println("Tempo de execução: " + (tempoExecucao / 1000000000) + " segundos");
 }
 
-}
+public void selectionsort() {
+    long contadorOperacoes = 0; 
+    long tempoInicio = System.nanoTime();
 
+    for (int i = 0; i < vetor.length - 1; ++i) {
+        int min = i;
+        for (int j = i + 1; j < vetor.length; ++j)
+            if (vetor[j] < vetor[min])
+                min = j;
+            int x = vetor[i];
+            vetor[i] = vetor[min];
+            vetor[min] = x;
+            contadorOperacoes++; 
+        }
+
+        long tempoFim = System.nanoTime(); 
+        long tempoExecucao = tempoFim - tempoInicio;
+    
+        System.out.println("Vetor ordenado: " + this.toString());
+    
+        System.out.println("Número de operações realizadas: " + contadorOperacoes);
+        System.out.println("Tempo de execução: " + (tempoExecucao / 1000000000) + " segundos");
+
+    }
+
+    public void insertionSort() {
+        long contadorOperacoes = 0; 
+        long tempoInicio = System.nanoTime();
+
+        for (int j = 1; j < vetor.length; ++j) {
+          int x = vetor[j];
+          int i;
+          for (i = j - 1; i >= 0 && vetor[i] > x; --i)
+            vetor[i + 1] = vetor[i];
+          vetor[i + 1] = x;
+          contadorOperacoes++; 
+        }
+
+        long tempoFim = System.nanoTime(); 
+        long tempoExecucao = tempoFim - tempoInicio;
+    
+        System.out.println("Vetor ordenado: " + this.toString());
+    
+        System.out.println("Número de operações realizadas: " + contadorOperacoes);
+        System.out.println("Tempo de execução: " + (tempoExecucao / 1000000000) + " segundos");
+
+      }
+    }
