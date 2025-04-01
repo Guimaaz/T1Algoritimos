2b4b7c343bfc36643193e76a9d4be7c62db61a77
Guimaaz <gustavoguimaraesr@gmail.com>
2025-04-01 17:38:38 -0300

    rodada de testes

--- NossoVetor.java
+++ NossoVetor.java
@@ -3,90 +3,83 @@ import java.util.Random;
 public class NossoVetor {
 
     private int[] vetor;
-    private int ocupacao;
+    private int tamanho;
 
-    public NossoVetor(int tamanho) {
-        vetor = new int[tamanho];
-        ocupacao = 0;
+    public NossoVetor(int capacidade) {
+        this.vetor = new int[capacidade];
+        this.tamanho = 0;
     }
 
-    public boolean estaVazio() {
-        return ocupacao == 0;
-    }
+    
 
-    public void preencheVetor() {
+    public void adicionarpreencher() {
         Random random = new Random();
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = random.nextInt(vetor.length * 1);
+        for (int i = 0; i < this.vetor.length; i++) {
+           this.vetor[i] = random.nextInt(this.vetor.length * 1);
 
         }
-        ocupacao = vetor.length;
+        this.tamanho = this.vetor.length;
     }
 
-    public String bubbleSort() {
-        long contadorTrocaBS = 0; // contador de trocas do BubbleSort
-        long contadorComparacaoBS = 0; // contador de comparações do BubbleSort
-        for (int i = 1; i < vetor.length; i++) {
-            for (int j = 0; j < vetor.length - i; j++) {
-                if (vetor[j] > vetor[j + 1]) {
-                    int aux = vetor[j];
-                    vetor[j] = vetor[j + 1];
-                    vetor[j + 1] = aux;
+    public long bubbleSort() {
+        long contadorBS = 0;
+        for (int i = 1; i < this.vetor.length; i++) {
+            for (int j = 0; j < this.vetor.length - i; j++) {
+                if (this.vetor[j] > this.vetor[j + 1]) {
+                    int aux = this.vetor[j];
+                    this.vetor[j] = this.vetor[j + 1];
+                    this.vetor[j + 1] = aux;
 
-                    contadorTrocaBS += 3;   
+                    contadorBS++;
                 }
-                contadorComparacaoBS++;
             }
         }
-        return "contador de trocas do bubbleSort: " + contadorTrocaBS + " || contador de comparação do bubbleSort: " + contadorComparacaoBS;
+        return contadorBS;
     }
 
-    // {3, 9, 4, 1, 2, 6, 5, 8, 10, 7}
-    public String selectionSort() {
-        long contadorTrocaSS = 0, contadorComparacaoSS = 0;
-        for (int i = 0; i < vetor.length - 1; i++) {
+    public long selectionSort() {
+        long contadorSS = 0;
+        for (int i = 0; i < this.vetor.length - 1; ++i) {
             int min = i;
-            for (int j = i + 1; j < vetor.length; j++) {
-                contadorComparacaoSS++;
-                if (vetor[min] > vetor[j]) {
+            for (int j = i + 1; j < this.vetor.length; ++j) {
+                if (this.vetor[j] < this.vetor[min]) {
                     min = j;
-                    contadorTrocaSS++;
                 }
+                int x = this.vetor[i];
+                this.vetor[i] = this.vetor[min];
+                this.vetor[min] = x;
+                if (this.vetor[i] != this.vetor[min]) {
+                    contadorSS++;
+                }
+
             }
-            int temp = vetor[min];
-            vetor[min] = vetor[i];
-            vetor[i] = temp;
-            contadorTrocaSS += 3;
         }
-        return "contador de trocas do SelectionSort: " + contadorTrocaSS + " || contador de comparação do SelectionSort: " + contadorComparacaoSS;
+        return contadorSS;
     }
 
-    // {3, 4, 9, 1, 2, 6, 5, 8, 10, 7}
-    public String insertionSort() {
-        long contadorTrocaIS = 0, contadorComparacaoIS = 0;
-        for (int i = 1; i < vetor.length; i++) {
-            int temp = vetor[i];
-            int j = i - 1;
-            while (j >= 0 && vetor[j] > temp) {
-                vetor[j + 1] = vetor[j];
-                j--;
-                contadorTrocaIS++;
-                contadorComparacaoIS++;
+    public long insertionSort() {
+        long contadorIS = 0;
+        for (int j = 1; j < this.vetor.length; ++j) {
+            int x = this.vetor[j];
+            int i;
+            for (i = j - 1; i >= 0 && this.vetor[i] > x; --i) {
+                this.vetor[i + 1] = this.vetor[i];
+                contadorIS++;
             }
-            vetor[j + 1] = temp;
-            contadorTrocaIS++;
+            this.vetor[i + 1] = x;
         }
-        return "contador de trocas do insertionSort: " + contadorTrocaIS + " || contador de comparação do insertionSort: " + contadorComparacaoIS;
+
+        return contadorIS;
     }
 
     public int buscaLinear(int elemento) {
-        int contadorLinear = 0; // Contador de comparações
+        int contadorLinear = 0;
 
-        for (int i = 0; i < vetor.length; i++) {
-            if (vetor[i] == elemento) {
-                // System.out.println(i); 
-
-                contadorLinear++; 
+        for (int i = 0; i < this.vetor.length; i++) {
+            contadorLinear++;
+            if (this.vetor[i] == elemento) {
+                // System.out.println(i);
+                return contadorLinear;
             }
         }
         return contadorLinear;
@@ -96,14 +89,15 @@ public class NossoVetor {
         int contadorBinario = 0; // Contador de comparações
 
         int inicio = 0;
-        int fim = vetor.length - 1;
+        int fim = this.vetor.length - 1;
         while (inicio <= fim) {
             int meio = (inicio + fim) / 2;
-            if (elemento == vetor[meio]) {
-                // contadorBinario++;      //   CORRIGIR O LOCAL ONDE É APLICADO O CONTADOR!!
-                return meio;
+            contadorBinario++;
+            if (elemento == this.vetor[meio]) {
+                return contadorBinario;
             }
-            if (elemento > vetor[meio]) {
+            contadorBinario++;
+            if (elemento > this.vetor[meio]) {
                 inicio = meio + 1;
             } else {
                 fim = meio - 1;
@@ -114,9 +108,9 @@ public class NossoVetor {
 
     @Override
     public String toString() {
-        String s = "ocupacao = " + ocupacao + "\n";
-        for (int i = 0; i < ocupacao; i++) {
-            s += vetor[i] + " ";
+        String s = "tamanho = " + tamanho + "\n";
+        for (int i = 0; i < tamanho; i++) {
+            s += this.vetor[i] + " ";
         }
         return s + "\n";
     }
--- TesteVetor.java
+++ TesteVetor.java
@@ -1,47 +1,37 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class TesteVetor {
     public static void main(String[] args) {
-        // NossoVetor vetorTeste = new NossoVetor(10);
-        // vetorTeste.preencheVetor();
-        // vetorTeste.bubbleSort();
-        // System.out.println(vetorTeste.buscaBinaria(7));
-        // System.out.println(vetorTeste);
+       
 
-        Scanner scanner = new Scanner(System.in);
-        NossoVetor vetorTeste;
-        int t;
-        int e;
-        int qtdTestes;
+        int tamanhoVetor = Integer.parseInt(JOptionPane.showInputDialog("qual o tamanho do vetor??"));
+        int numeroProcurado = Integer.parseInt(JOptionPane.showInputDialog("qual o numero a ser buscado??"));
+        int numeroDeTestes = Integer.parseInt(JOptionPane.showInputDialog("qual o numero de testes"));
+        NossoVetor vetor = new NossoVetor(tamanhoVetor);
+        
+        
+        
 
         
-        System.out.println("Tamanho do Vetor: "); // definir o tamanho do vetor
-        t = scanner.nextInt();
-        System.out.println("Elemento a ser buscado: "); // Definir elemento de busca
-        e = scanner.nextInt();
-        System.out.println("Quantidade de Testes: "); // 
-        qtdTestes = scanner.nextInt();
 
-        for (int i = 1; i < qtdTestes + 1; i++) {
-            vetorTeste = new NossoVetor(t);
-            System.out.printf("\nBateria de Testes %d \n", i);
+        for (int i = 0; i < numeroDeTestes; i++) {
+            
+            System.out.printf("teste numero",  i);
 
-            vetorTeste.preencheVetor();
-            System.out.println(vetorTeste.buscaLinear(e));  // busca linear antes de ordenar o vetor
-            System.out.println(vetorTeste.bubbleSort());
-            System.out.println(vetorTeste.buscaBinaria(e) + "\n");   // busca binária depois de ordenar o valor 
+            vetor.adicionarpreencher();
+            System.out.println(vetor.buscaLinear(numeroProcurado)); 
+            System.out.println(vetor.bubbleSort());
+            System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");  
 
-            vetorTeste.preencheVetor();
-            System.out.println(vetorTeste.buscaLinear(e));
-            System.out.println(vetorTeste.selectionSort());
-            System.out.println(vetorTeste.buscaBinaria(e) + "\n");
+            // vetor.adicionarpreencher();
+            // System.out.println(vetor.buscaLinear(numeroProcurado));
+            // System.out.println(vetor.selectionSort());
+            // System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");
 
-            vetorTeste.preencheVetor();
-            System.out.println(vetorTeste.buscaLinear(e));
-            System.out.println(vetorTeste.insertionSort());
-            System.out.println(vetorTeste.buscaBinaria(e) + "\n");
+            // vetor.adicionarpreencher();
+            // System.out.println(vetor.buscaLinear(numeroProcurado));
+            // System.out.println(vetor.insertionSort());
+            // System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");
+
         }
-
-        scanner.close();
-
     }
 }
