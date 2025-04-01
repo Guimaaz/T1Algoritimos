5e24715211169c4c8c09fce3bc1a1af270f123d7
Guimaaz <gustavoguimaraesr@gmail.com>
2025-04-01 17:43:36 -0300

    comeco de teste

--- TesteVetor.java
+++ TesteVetor.java
@@ -1,37 +1,20 @@
 import javax.swing.JOptionPane;
+
 public class TesteVetor {
     public static void main(String[] args) {
-       
-
-        int tamanhoVetor = Integer.parseInt(JOptionPane.showInputDialog("qual o tamanho do vetor??"));
-        int numeroProcurado = Integer.parseInt(JOptionPane.showInputDialog("qual o numero a ser buscado??"));
-        int numeroDeTestes = Integer.parseInt(JOptionPane.showInputDialog("qual o numero de testes"));
-        NossoVetor vetor = new NossoVetor(tamanhoVetor);
-        
-        
-        
-
-        
+        int tamanhoVetor = Integer.parseInt(JOptionPane.showInputDialog("Qual o tamanho do vetor?"));
+        int numeroProcurado = Integer.parseInt(JOptionPane.showInputDialog("Qual o número a ser buscado?"));
+        int numeroDeTestes = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de testes?"));
 
         for (int i = 0; i < numeroDeTestes; i++) {
-            
-            System.out.printf("teste numero",  i);
+            NossoVetor vetor = new NossoVetor(tamanhoVetor);
+
+            System.out.printf("Teste número %d\n", i + 1);
 
             vetor.adicionarpreencher();
             System.out.println(vetor.buscaLinear(numeroProcurado)); 
             System.out.println(vetor.bubbleSort());
-            System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");  
-
-            // vetor.adicionarpreencher();
-            // System.out.println(vetor.buscaLinear(numeroProcurado));
-            // System.out.println(vetor.selectionSort());
-            // System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");
-
-            // vetor.adicionarpreencher();
-            // System.out.println(vetor.buscaLinear(numeroProcurado));
-            // System.out.println(vetor.insertionSort());
-            // System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");
-
+            System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");
         }
     }
 }
