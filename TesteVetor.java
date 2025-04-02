af316b1bc3b8db4876373389862cd2ea7d3e86da
Guimaaz <Gustavoguimaraesr@gmail.com>
2025-04-02 18:18:59 -0300

    commit 800k insertion

--- TesteVetor.java
+++ TesteVetor.java
@@ -15,11 +15,11 @@ public class TesteVetor {
             vetor.adicionarpreencher();
             long ini = Calendar.getInstance().getTimeInMillis();
             System.out.println(vetor.buscaLinear(numeroProcurado)); 
-            System.out.println(vetor.bubbleSort());
-            vetor.bubbleSort();
+            System.out.println(vetor.insertionSort());
+            vetor.insertionSort();
             long fim = Calendar.getInstance().getTimeInMillis();
             System.out.println(vetor.buscaBinaria(numeroProcurado) + "\n");
-            System.out.println("bubble demorou " + (fim-ini) + " milissegundos");
+            System.out.println("insertion demorou " + (fim-ini) + " milissegundos");
         }
     }
 }
