2a55a8f210ebf5f33a8257b76f8d9825005f1040
GuiMD <gmdurt14@gmail.com>
2025-04-04 14:27:18 -0300

    Organização dos dados

---
+++ graficos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Plano:
+# Criar quatro gráficos. 1 para Bubble, 1 para Insertion e 1 para selection.
+# cada gráfico vai ter 1 reta referente ao processo em relação ao tempo
+# 4 gráfico vai ser no geral para podemos ver as retas ao mesmo tempo.
